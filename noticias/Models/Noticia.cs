f46a4bebe6c163a319740344835f3c21070c1661
Giovana Laudi Cardoso <80963852+GioLaudi@users.noreply.github.com>
2022-05-04 18:53:35 -0300

    gi/inclusaoDeClasses (#4)
    * inclusao das classes e ajuste no program e startup
    
    * Ajuste nos tamanhos dos campos
    
    Co-authored-by: Giovana Laudi Cardoso <giovana.cardoso@olxbr.net>

---
+++ noticias/Models/Noticia.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace noticias.Models
+{
+    [Table("Noticia")]
+    public class Noticia
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength( maximumLength: 100, MinimumLength = 2, ErrorMessage = "O {0} deve ter no minimo {2} e no máximo {1}")]
+        public string Titulo { get; set; }
+
+        public string Texto { get; set; }
+
+        [StringLength(100, ErrorMessage = "O {0} deve ter no máximo {1}")]
+        public string Imagem { get; set; }
+
+        public DateTime DtInclusao { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+    }
+}
---
+++ noticias/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace noticias.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(maximumLength: 80, MinimumLength = 2, ErrorMessage = "O {0} deve ter no minimo {2} e no máximo {1}")]
+        public string Nome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1}")]
+        public string Email { get; set; }
+
+        [StringLength(maximumLength: 30, MinimumLength = 6, ErrorMessage = "O {0} deve ter no minimo {2} e no máximo {1}")]
+        public string Senha { get; set; }
+
+        public List<Noticia> Noticias { get; set; }
+    }
+}
