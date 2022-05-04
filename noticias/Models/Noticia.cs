99aca8b5fcf0cd5115820275c12fbca3a12aadbe
Giovana Laudi Cardoso <giovana.cardoso@olxbr.net>
2022-05-03 22:48:28 -0300

    inclusao das classes e ajuste no program e startup

---
+++ noticias/Models/Noticia.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace noticias.Models
+{
+    [Table("Noticia")]
+    public class Noticia
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O {0} deve ter no minimo {1} e no máximo {2}")]
+        public string Titulo { get; set; }
+
+        [StringLength(800, MinimumLength = 10, ErrorMessage = "O {0} deve ter no minimo {1} e no máximo {2}")]
+        public string Texto { get; set; }
+
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "O {0} deve ter no minimo {1} e no máximo {2}")]
+        public string Imagem { get; set; }
+
+        public DateTime DtInclusao { get; set; }
+
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+    }
+}
---
+++ noticias/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace noticias.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "O {0} deve ter no minimo {1} e no máximo {2}")]
+        public string Nome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O {0} deve ter no minimo {1} e no máximo {2}")]
+        public string Email { get; set; }
+
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "O {0} deve ter no minimo {1} e no máximo {2}")]
+        public string Senha { get; set; }
+
+        public List<Noticia> Noticias { get; set; }
+    }
+}
