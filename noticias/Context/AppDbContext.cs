50d4d65d5c76fca16bf2a6863d0dfa04737cf407
Gabriel <gabrielsjanuario@gmail.com>
2022-05-04 20:42:52 -0300

    adicionado entity e configuracao do entity

---
+++ noticias/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using noticias.Models;
+
+namespace noticias.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Noticia> Noticias { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ noticias/Migrations/20220504233457_PopularUsuario.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace noticias.Migrations
+{
+    public partial class PopularUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT Into Usuario(Nome,Email,Senha)" +
+                "Values ('Giovana','testeemailinicial@teste.com','senhaboa')");
+
+            migrationBuilder.Sql("INSERT Into Usuario(Nome,Email,Senha)" +
+                "Values ('Gabriel','testeemailinicial2@teste.com','senhamuitoboa')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE from Usuario");
+        }
+    }
+}
