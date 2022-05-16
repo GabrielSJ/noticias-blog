ee9cff9ebab9f7aaece1d1e8c57350c893246d48
Gabriel Strada Januario <41273294+GabrielSJ@users.noreply.github.com>
2022-05-16 20:59:17 -0300

    criado view e adicionado imagens (#8)

---
+++ noticias/Controllers/NoticiaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using noticias.Repositories.Interfaces;
+
+namespace noticias.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        private readonly INoticiaRepository _NoticiaRepository;
+
+        public NoticiaController(INoticiaRepository noticiaRepository)
+        {
+            _NoticiaRepository = noticiaRepository;
+        }
+
+        public IActionResult List()
+        {
+            var noticias = _NoticiaRepository.Noticias;
+            return View(noticias);
+        }
+    }
+}
---
+++ noticias/Migrations/20220512010220_AddImagensUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace noticias.Migrations
+{
+    public partial class AddImagensUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Update Noticia set imagem='https://www.encurtador.com.br/alrB3'" +
+                "where id=1");
+            migrationBuilder.Sql("Update Noticia set imagem='https://www.encurtador.com.br/flmrM'" +
+                "where id=2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
