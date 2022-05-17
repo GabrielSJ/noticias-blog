99dd5d6f8e41d0ed81f55674a05e3fa7cf51245a
Giovana Laudi Cardoso <giovana.cardoso@olxbr.net>
2022-05-17 19:14:34 -0300

    ajustes

---
+++ noticias/Migrations/20220517220912_atualizarTamanhoTabela.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace noticias.Migrations
+{
+    public partial class atualizarTamanhoTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Alter Table Noticia Alter Column Imagem nvarchar(3000)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
