6af6a28c23c3ce2d93eff7e8d641dbc0a11ab4fc
Gabriel <gabrielsjanuario@gmail.com>
2022-05-19 20:52:04 -0300

    criado controler e view para fazer o login

---
+++ noticias/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using noticias.ViewModels;
+
+namespace noticias.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel objLoginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(objLoginViewModel);
+            }
+
+            if (objLoginViewModel != null)
+            {
+                var user = await _userManager.FindByNameAsync(objLoginViewModel.Email);
+
+                if (user != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, objLoginViewModel.Senha, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        if (string.IsNullOrEmpty(objLoginViewModel.ReturnUrl))
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                        return Redirect(objLoginViewModel.ReturnUrl);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Dados não repassados");
+                return View(objLoginViewModel);
+            }
+
+            ModelState.AddModelError("", "Falha ao realizar o login!");
+            return View(objLoginViewModel);
+        }
+    }
+}
---
+++ noticias/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace noticias.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Informa o email")]
+        [Display(Name = "Usuario")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Senha { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
